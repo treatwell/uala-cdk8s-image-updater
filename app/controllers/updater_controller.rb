c28bd9c4e8868810a082cfd6a3812f364bcf521b
jeko <jeko@jeko.net>
2023-09-14 13:25:37 +0200

    Add ENV['DEBUG'] and small refactor

--- app/controllers/updater_controller.rb
+++ app/controllers/updater_controller.rb
@@ -13,7 +13,7 @@ class UpdaterController
   def initialize
     @current_step = 0
     @git_client = nil
-    @applications_conf = []
+    @applications_conf = {}
     @applications_to_update = []
   end
 
@@ -30,6 +30,7 @@ class UpdaterController
 
   def check_required_params
     _announce_step "Check required params..."
+
     unless ENV.key?('GIT_IAC_REPO')
       puts '[ERROR] GIT_IAC_REPO environment variable is missing!'.red
       exit 1
@@ -59,6 +60,7 @@ class UpdaterController
     puts "\n#################################################################".green
     puts "\nWe are looking for:\n"
     puts "GIT REPO: #{ENV['GIT_SOURCE_REPO'].green}"
+
     if ENV.key?('GIT_SOURCE_BRANCH')
       puts "GIT BRANCH: #{ENV['GIT_SOURCE_BRANCH'].green}"
     end
@@ -72,34 +74,39 @@ class UpdaterController
     elsif ENV.key?('GIT_SOURCE_COMMIT_SHA')
       puts "\nWe want to update images to commit: #{ENV['GIT_SOURCE_COMMIT_SHA'].green}"
     end
+
     puts "\nWe are updating IAC REPO: #{ENV['GIT_IAC_REPO'].green}"
+
     if ENV.key?('GIT_IAC_BRANCH')
       puts "ON BRANCH: #{ENV['GIT_IAC_BRANCH'].green}"
     end
+
     puts "\n#################################################################".green
   end
 
   def prepare_local_environment
     _announce_step "Prepare environment..."
+
     ENV['GITHUB_TOKEN'] = ENV['GIT_IAC_TOKEN']
     FileUtils.rm_rf('iac-repo')
+
     puts 'OK.'
   end
 
   def clone_iac_repo
     _announce_step "Clone iac-repo..."
+
     @git_client = Git.clone(
       "https://#{ENV['GIT_IAC_TOKEN']}@#{ENV['GIT_IAC_REPO']}",
       'iac-repo',
       { branch: ENV['GIT_IAC_BRANCH'] }
     )
+
     puts 'OK.'
   end
 
   def get_applications_available
     _announce_step "Get applications available in iac-repo..."
-
-    @applications_conf = {}
 
     Dir.glob('iac-repo/applications*.yaml').each do |file|
       File.open(file, 'r') do |yaml_file|
@@ -108,14 +115,13 @@ class UpdaterController
       end
     end
 
-    # puts @applications_conf.to_yaml
+    puts @applications_conf.to_yaml if ENV['DEBUG'] == 'true'
     puts 'OK.'
   end
 
   def find_involved_applications
     _announce_step "Find applications that match the current repo and branch..."
 
-    @applications_to_update = []
     Utilities.check_type(@applications_conf, '', @applications_to_update)
 
     if @applications_to_update.count == 0
@@ -133,15 +139,18 @@ class UpdaterController
     _announce_step "Update applications images files with new tags..."
 
     @applications_to_update.each do |app|
-      # puts app
+      puts app if ENV['DEBUG'] == 'true'
+
       File.open("iac-repo/applications/#{app['path']}/applications_settings.yaml", 'r+') do |yaml_file|
         yaml_content = YAML.safe_load(yaml_file.read)
-        # puts app
+
         if yaml_content[app['name']]
           yaml_content[app['name']]['image'] = app['image']
           print "#{app['path']} - #{app['name']}: #{app['image'].green}\n"
         end
-        # puts yaml_content.to_yaml
+
+        puts yaml_content.to_yaml if ENV['DEBUG'] == 'true'
+
         yaml_file.rewind
         yaml_file.write(yaml_content.to_yaml)
         yaml_file.truncate(yaml_file.pos)
@@ -153,16 +162,19 @@ class UpdaterController
     _announce_step "Push edited files to iac repo..."
 
     push_branch = "#{ENV['GIT_SOURCE_REPO']}-#{ENV['GIT_SOURCE_BRANCH']}"
+
     if ENV.key?('GIT_SOURCE_TAG')
       push_branch += "-#{ENV['GIT_SOURCE_TAG']}"
     else
       push_branch += "-#{ENV['GIT_SOURCE_COMMIT_SHA']}"
     end
+
     push_branch += "-#{DateTime.now.strftime('%s')}"
 
     if (ENV.key?('GIT_SOURCE_TAG') &&
        (!ENV.key?('GIT_IAC_DISABLE_TAG_PR') || ENV['GIT_IAC_DISABLE_TAG_PR'] != 'true')) ||
        ENV['GIT_IAC_FORCE_PR'] == 'true'
+
       if ENV['GIT_DRY_RUN'] == 'true'
         puts "\n[DRY_RUN] Added edited files to a new branch: #{push_branch.green}."
         puts "\n[DRY_RUN] No PR will be created in dryrun mode."
@@ -177,7 +189,9 @@ class UpdaterController
       puts "\nAdded edited files to a new branch: #{push_branch.green}."
       puts "\nCreating a PR..."
       puts `cd iac-repo && gh pr create #{ENV.key?('GIT_IAC_BRANCH') ? '--base ' + ENV['GIT_IAC_BRANCH'] : ''} --title "[IMAGE_UPDATER] #{push_branch}" --body ""`
+
     else
+
       if ENV['GIT_DRY_RUN'] == 'true'
         puts "\n[DRY_RUN] Edited files have been pushed to #{@git_client.current_branch} branch.".green
         return
@@ -186,6 +200,7 @@ class UpdaterController
       on_retry = proc do |exception, try, elapsed_time, next_interval|
         puts "#{exception.class}: '#{exception.message}' - #{try} tries in #{elapsed_time} seconds and #{next_interval} seconds until the next try.".red
       end
+
       begin
         @git_client.checkout(@git_client.current_branch)
         @git_client.commit_all("[IMAGE_UPDATER] #{push_branch}")
--- app/utilities/updater_utilities.rb
+++ app/utilities/updater_utilities.rb
@@ -14,21 +14,21 @@ module Utilities
     git_repo_found = false
     git_branch_found = false
     git_only_tags_found = false
-    hash.each_pair do |k, v|
-      # puts "HASH: #{k}"
-      if k == 'git_repo' && v == ENV['GIT_SOURCE_REPO']
-        git_repo_found = true
+
+    hash.each_pair do |key, value|
+      puts "HASH: #{key}" if ENV['DEBUG'] == 'true'
+
+      case key
+      when 'git_repo'
+        git_repo_found = true if value == ENV['GIT_SOURCE_REPO']
+      when 'branch'
+        regexp = Regexp.new("^#{value}$")
+        git_branch_found = true if regexp.match?(ENV['GIT_SOURCE_BRANCH'])
+      when 'only_tags'
+        git_only_tags_found = true if value == true
       end
-      if k == 'branch'
-        regexp = Regexp.new("^#{v}$")
-        if regexp.match?(ENV['GIT_SOURCE_BRANCH'])
-          git_branch_found = true
-        end
-      end
-      if k == 'only_tags' && v == true
-        git_only_tags_found = true
-      end
-      check_type(v, "#{path}/#{k}", applications_to_update)
+
+      check_type(value, "#{path}/#{key}", applications_to_update)
     end
 
     if git_repo_found &&
@@ -40,13 +40,15 @@ module Utilities
       )
       hash['path'] = path
       applications_to_update.push(hash)
-      # puts "APP FOUND: \npath:#{path}\n#{hash.to_yaml}\n\n"
+
+      puts "APP FOUND: \npath:#{path}\n#{hash.to_yaml}\n\n"  if ENV['DEBUG'] == 'true'
     end
   end
 
   def iArray(array, path, applications_to_update)
     array.each do |v|
-      # puts "ARRAY!" #{v}"
+      puts "ARRAY! #{v}"  if ENV['DEBUG'] == 'true'
+
       check_type(v, path, applications_to_update)
     end
     path
