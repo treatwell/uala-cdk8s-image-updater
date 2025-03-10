06d807cdb0a16b727f471554b54b3a8d4d64b985
Daniel Goberitz <daniel.goberitz@treatwell.com>
2025-03-10 18:28:18 +0100

    test: refactor specs to use Given-When-Then format
    - Add explicit Given-When-Then sections
    - Improve test organization and readability
    - Add more descriptive context blocks
    - Part of SOAI-346

--- spec/unit/utilities/updater_utilities_spec.rb
+++ spec/unit/utilities/updater_utilities_spec.rb
@@ -10,138 +10,197 @@ RSpec.describe Utilities do
   end
 
   describe '.check_type' do
+    # Holds the array where matched applications will be stored
     let(:applications_to_update) { [] }
 
     context 'with development deployment configuration' do
-      let(:config) do
-        {
-          'environments' => [
-            {
-              'develop' => [
-                {
-                  'name' => 'testApp',
-                  'git_repo' => 'uala/test-app',
-                  'branch' => 'main',
-                  'image' => 'org/test-app:main-12345678'
-                }
-              ]
-            }
-          ]
-        }
-      end
-
-      it 'finds matching applications by repo and branch' do
-        with_env(base_env) do
-          described_class.check_type(config, '', applications_to_update)
-          expect(applications_to_update.size).to eq(1)
-          expect(applications_to_update.first).to include(
-            'name' => 'testApp',
-            'git_repo' => 'uala/test-app'
-          )
+      describe 'matching applications by repo and branch' do
+        # Given
+        let(:config) do
+          {
+            'environments' => [
+              {
+                'develop' => [
+                  {
+                    'name' => 'testApp',
+                    'git_repo' => 'uala/test-app',
+                    'branch' => 'main',
+                    'image' => 'org/test-app:main-12345678'
+                  }
+                ]
+              }
+            ]
+          }
         end
-      end
 
-      it 'does not match when branch differs' do
-        with_env(base_env.merge('GIT_SOURCE_BRANCH' => 'feature')) do
-          described_class.check_type(config, '', applications_to_update)
-          expect(applications_to_update).to be_empty
+        context 'when branch matches' do
+          it 'finds the matching application' do
+            # Given
+            env_vars = base_env
+
+            # When
+            with_env(env_vars) do
+              described_class.check_type(config, '', applications_to_update)
+            end
+
+            # Then
+            expect(applications_to_update.size).to eq(1)
+            expect(applications_to_update.first).to include(
+              'name' => 'testApp',
+              'git_repo' => 'uala/test-app'
+            )
+          end
+        end
+
+        context 'when branch differs' do
+          it 'does not find any applications' do
+            # Given
+            env_vars = base_env.merge('GIT_SOURCE_BRANCH' => 'feature')
+
+            # When
+            with_env(env_vars) do
+              described_class.check_type(config, '', applications_to_update)
+            end
+
+            # Then
+            expect(applications_to_update).to be_empty
+          end
         end
       end
     end
 
     context 'with production deployment configuration' do
-      let(:config) do
-        {
-          'environments' => [
-            {
-              'production' => [
-                {
-                  'name' => 'testApp',
-                  'git_repo' => 'uala/test-app',
-                  'branch' => 'main',
-                  'only_tags' => true,
-                  'image' => 'org/test-app:v1.0.0'
-                }
-              ]
-            }
-          ]
-        }
-      end
-
-      it 'finds matching applications when tag is present' do
-        with_env(base_env.merge('GIT_SOURCE_TAG' => 'v1.0.0')) do
-          described_class.check_type(config, '', applications_to_update)
-          expect(applications_to_update.size).to eq(1)
-          expect(applications_to_update.first).to include(
-            'name' => 'testApp',
-            'only_tags' => true
-          )
+      describe 'handling tag-based deployments' do
+        # Given
+        let(:config) do
+          {
+            'environments' => [
+              {
+                'production' => [
+                  {
+                    'name' => 'testApp',
+                    'git_repo' => 'uala/test-app',
+                    'branch' => 'main',
+                    'only_tags' => true,
+                    'image' => 'org/test-app:v1.0.0'
+                  }
+                ]
+              }
+            ]
+          }
         end
-      end
 
-      it 'does not match when only_tags is true but no tag is present' do
-        with_env(base_env) do
-          described_class.check_type(config, '', applications_to_update)
-          expect(applications_to_update).to be_empty
+        context 'when tag is present' do
+          it 'finds the matching application' do
+            # Given
+            env_vars = base_env.merge('GIT_SOURCE_TAG' => 'v1.0.0')
+
+            # When
+            with_env(env_vars) do
+              described_class.check_type(config, '', applications_to_update)
+            end
+
+            # Then
+            expect(applications_to_update.size).to eq(1)
+            expect(applications_to_update.first).to include(
+              'name' => 'testApp',
+              'only_tags' => true
+            )
+          end
+        end
+
+        context 'when no tag is present' do
+          it 'does not find any applications' do
+            # Given
+            env_vars = base_env
+
+            # When
+            with_env(env_vars) do
+              described_class.check_type(config, '', applications_to_update)
+            end
+
+            # Then
+            expect(applications_to_update).to be_empty
+          end
         end
       end
     end
 
     context 'with force update application' do
-      let(:config) do
-        {
-          'environments' => [
-            {
-              'develop' => [
-                {
-                  'name' => 'testApp',
-                  'git_repo' => 'uala/test-app',
-                  'branch' => 'main'
-                },
-                {
-                  'name' => 'otherApp',
-                  'git_repo' => 'uala/test-app',
-                  'branch' => 'main'
-                }
-              ]
-            }
-          ]
-        }
-      end
+      describe 'handling forced application updates' do
+        # Given
+        let(:config) do
+          {
+            'environments' => [
+              {
+                'develop' => [
+                  {
+                    'name' => 'testApp',
+                    'git_repo' => 'uala/test-app',
+                    'branch' => 'main'
+                  },
+                  {
+                    'name' => 'otherApp',
+                    'git_repo' => 'uala/test-app',
+                    'branch' => 'main'
+                  }
+                ]
+              }
+            ]
+          }
+        end
 
-      it 'only includes forced application when FORCE_UPDATE_APP is set' do
-        with_env(base_env.merge('FORCE_UPDATE_APP' => 'testApp')) do
-          described_class.check_type(config, '', applications_to_update)
-          expect(applications_to_update.size).to eq(1)
-          expect(applications_to_update.first['name']).to eq('testApp')
+        context 'when FORCE_UPDATE_APP is set' do
+          it 'only includes the forced application' do
+            # Given
+            env_vars = base_env.merge('FORCE_UPDATE_APP' => 'testApp')
+
+            # When
+            with_env(env_vars) do
+              described_class.check_type(config, '', applications_to_update)
+            end
+
+            # Then
+            expect(applications_to_update.size).to eq(1)
+            expect(applications_to_update.first['name']).to eq('testApp')
+          end
         end
       end
     end
 
     context 'with nested configuration structure' do
-      let(:config) do
-        {
-          'environments' => [
-            {
-              'develop' => [
-                {
-                  'services' => [
-                    {
-                      'name' => 'testApp',
-                      'git_repo' => 'uala/test-app',
-                      'branch' => 'main'
-                    }
-                  ]
-                }
-              ]
-            }
-          ]
-        }
-      end
+      describe 'traversing nested configurations' do
+        # Given
+        let(:config) do
+          {
+            'environments' => [
+              {
+                'develop' => [
+                  {
+                    'services' => [
+                      {
+                        'name' => 'testApp',
+                        'git_repo' => 'uala/test-app',
+                        'branch' => 'main'
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+        end
 
-      it 'traverses nested structures to find applications' do
-        with_env(base_env) do
-          described_class.check_type(config, '', applications_to_update)
+        it 'finds applications in nested structures' do
+          # Given
+          env_vars = base_env
+
+          # When
+          with_env(env_vars) do
+            described_class.check_type(config, '', applications_to_update)
+          end
+
+          # Then
           expect(applications_to_update.size).to eq(1)
           expect(applications_to_update.first['name']).to eq('testApp')
         end
