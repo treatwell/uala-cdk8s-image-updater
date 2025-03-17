e96bcf60cccaaa7bf18df1f31b99e8b3f1bbfe78
Kirill <3367894@gmail.com>
2025-03-17 15:59:45 +0100

    SOAI-347: small changes for utilities spec

--- spec/unit/utilities/updater_utilities_spec.rb
+++ spec/unit/utilities/updater_utilities_spec.rb
@@ -1,149 +1,103 @@
 require 'spec_helper'
 
 RSpec.describe Utilities do
-  let(:base_env) do
-    {
+  before do
+    stub_env(
       'GIT_SOURCE_REPO' => 'uala/test-app',
       'GIT_SOURCE_BRANCH' => 'main',
       'GIT_SOURCE_COMMIT_SHA' => '12345678'
-    }
+    )
   end
 
   describe '.check_type' do
     # Holds the array where matched applications will be stored
     let(:applications_to_update) { [] }
+    let(:config) { YAML.load_file(config_file_name) }
 
-    context 'with development deployment configuration' do
-      describe 'matching applications by repo and branch' do
+    describe 'matching applications by repo and branch' do
+      # Given
+      let(:config_file_name) { 'spec/fixtures/development/basic.yaml' }
+
+      it 'finds the matching application when branch matches' do
+        # When
+        described_class.check_type(config, '', applications_to_update)
+
+        # Then
+        expect(applications_to_update.size).to eq(1)
+        expect(applications_to_update.first).to include(
+                                                  'name' => 'testApp',
+                                                  'git_repo' => 'uala/test-app'
+                                                )
+      end
+
+      it 'does not find any applications when branch differs' do
         # Given
-        let(:config) do
-          YAML.load_file('spec/fixtures/development/basic.yaml')
-        end
+        stub_env('GIT_SOURCE_BRANCH', 'feature')
 
-        context 'when branch matches' do
-          it 'finds the matching application' do
-            # Given
-            env_vars = base_env
+        # When
+        described_class.check_type(config, '', applications_to_update)
 
-            # When
-            with_env(env_vars) do
-              described_class.check_type(config, '', applications_to_update)
-            end
-
-            # Then
-            expect(applications_to_update.size).to eq(1)
-            expect(applications_to_update.first).to include(
-              'name' => 'testApp',
-              'git_repo' => 'uala/test-app'
-            )
-          end
-        end
-
-        context 'when branch differs' do
-          it 'does not find any applications' do
-            # Given
-            env_vars = base_env.merge('GIT_SOURCE_BRANCH' => 'feature')
-
-            # When
-            with_env(env_vars) do
-              described_class.check_type(config, '', applications_to_update)
-            end
-
-            # Then
-            expect(applications_to_update).to be_empty
-          end
-        end
+        # Then
+        expect(applications_to_update).to be_empty
       end
     end
 
-    context 'with production deployment configuration' do
-      describe 'handling tag-based deployments' do
+    describe 'handling tag-based deployments' do
+      # Given
+      let(:config_file_name) { 'spec/fixtures/production/tag_based.yaml' }
+
+      it 'finds the matching application when tag is present' do
         # Given
-        let(:config) do
-          YAML.load_file('spec/fixtures/production/tag_based.yaml')
-        end
+        stub_env('GIT_SOURCE_TAG', 'v1.0.0')
 
-        context 'when tag is present' do
-          it 'finds the matching application' do
-            # Given
-            env_vars = base_env.merge('GIT_SOURCE_TAG' => 'v1.0.0')
+        # When
+        described_class.check_type(config, '', applications_to_update)
 
-            # When
-            with_env(env_vars) do
-              described_class.check_type(config, '', applications_to_update)
-            end
+        # Then
+        expect(applications_to_update.size).to eq(1)
+        expect(applications_to_update.first).to include(
+                                                  'name' => 'testApp',
+                                                  'only_tags' => true
+                                                )
+      end
 
-            # Then
-            expect(applications_to_update.size).to eq(1)
-            expect(applications_to_update.first).to include(
-              'name' => 'testApp',
-              'only_tags' => true
-            )
-          end
-        end
+      it 'does not find any applications when no tag is present' do
+        # When
+        described_class.check_type(config, '', applications_to_update)
 
-        context 'when no tag is present' do
-          it 'does not find any applications' do
-            # Given
-            env_vars = base_env
-
-            # When
-            with_env(env_vars) do
-              described_class.check_type(config, '', applications_to_update)
-            end
-
-            # Then
-            expect(applications_to_update).to be_empty
-          end
-        end
+        # Then
+        expect(applications_to_update).to be_empty
       end
     end
 
-    context 'with force update application' do
-      describe 'handling forced application updates' do
+    describe 'handling forced application updates' do
+      # Given
+      let(:config_file_name) { 'spec/fixtures/development/multiple_apps.yaml' }
+
+      it 'only includes the forced application when FORCE_UPDATE_APP is set' do
         # Given
-        let(:config) do
-          YAML.load_file('spec/fixtures/development/multiple_apps.yaml')
-        end
+        stub_env('FORCE_UPDATE_APP', 'testApp')
 
-        context 'when FORCE_UPDATE_APP is set' do
-          it 'only includes the forced application' do
-            # Given
-            env_vars = base_env.merge('FORCE_UPDATE_APP' => 'testApp')
+        # When
+        described_class.check_type(config, '', applications_to_update)
 
-            # When
-            with_env(env_vars) do
-              described_class.check_type(config, '', applications_to_update)
-            end
-
-            # Then
-            expect(applications_to_update.size).to eq(1)
-            expect(applications_to_update.first['name']).to eq('testApp')
-          end
-        end
+        # Then
+        expect(applications_to_update.size).to eq(1)
+        expect(applications_to_update.first['name']).to eq('testApp')
       end
     end
 
-    context 'with nested configuration structure' do
-      describe 'traversing nested configurations' do
-        # Given
-        let(:config) do
-          YAML.load_file('spec/fixtures/nested/services.yaml')
-        end
+    describe 'traversing nested configurations' do
+      # Given
+      let(:config_file_name) { 'spec/fixtures/nested/services.yaml' }
 
-        it 'finds applications in nested structures' do
-          # Given
-          env_vars = base_env
+      it 'finds applications in nested structures' do
+        # When
+        described_class.check_type(config, '', applications_to_update)
 
-          # When
-          with_env(env_vars) do
-            described_class.check_type(config, '', applications_to_update)
-          end
-
-          # Then
-          expect(applications_to_update.size).to eq(1)
-          expect(applications_to_update.first['name']).to eq('testApp')
-        end
+        # Then
+        expect(applications_to_update.size).to eq(1)
+        expect(applications_to_update.first['name']).to eq('testApp')
       end
     end
   end
